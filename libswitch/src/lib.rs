9fbe55d4342906496a886c2a8adad4f964cb0913
Valentin B <valentin.be@protonmail.com>
2019-10-11 16:17:00 +0200

    Define libswitch crate and add it to workspace

---
+++ libswitch/src/lib.rs
@@ -0,0 +1,7 @@
+//! Low-level hardware access library for the Nintendo Switch.
+//!
+//! **Note:** This code is written specifically for the Switch.
+//! If you decide to use it for other Tegra210 plattforms, use
+//! at own risk.
+
+#![no_std]
