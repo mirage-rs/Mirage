a8252f4a332b8b1c6f0580cf07bf5a6690f649d9
Valentin B <valentin.be@protonmail.com>
2019-10-31 16:26:00 +0100

    [libswitch] Refactor timer interface

--- libswitch/src/timer/mod.rs
+++ libswitch/src/timer/mod.rs
@@ -1,55 +1,90 @@
-//! Interface to the Tegra210 Real-Time Clock and the timers.
+//! Interface to the Tegra210 RTC and the timers.
+//!
+//! # Description
+//!
+//! The Real-Time Clock (RTC) module maintains seconds and milliseconds counters,
+//! and five alarm registers. The RTC is in the 'always on' power domain, allowing
+//! for the counters to run and alarms to trigger when the system is in low-power
+//! state. If configured, interrupts triggered by the RTC can cause the system to
+//! wake up from a low-power state.
+//!
+//! The Fixed Time Base Registers meanwhile provide a fixed time base in
+//! microseconds to be used by the rest of the system regardless of the
+//! clk_m frequency.
+//!
+//! # Example
+//!
+//! ```
+//! use mirage_libswitch::timer::sleep;
+//!
+//! fn main() {
+//!     sleep(5);
+//!     println!("Five seconds later.");
+//! }
+//! ```
 
 use register::mmio::ReadWrite;
 
-pub mod watchdog;
+/// Base address for Timer registers.
+const TIMERS_BASE: u32 = 0x6000_5000;
+
+/// The `TIMERUS_CNTR_1US_0` register.
+const TIMERUS_CNTR_1US: &'static ReadWrite<u32> =
+    unsafe { &(*((TIMERS_BASE + 0x10) as *const ReadWrite<u32>)) };
+
+/// Base address for RTC registers.
+const RTC_BASE: u32 = 0x7000_E000;
+
+/// The `APBDEV_RTC_SECONDS_0` register.
+const RTC_SECONDS: &'static ReadWrite<u32> =
+    unsafe { &(*((RTC_BASE + 0x8) as *const ReadWrite<u32>)) };
+
+/// The `APBDEV_RTC_SHADOW_SECONDS_0` register.
+const RTC_SHADOW_SECONDS: &'static ReadWrite<u32> =
+    unsafe { &(*((RTC_BASE + 0xC) as *const ReadWrite<u32>)) };
+
+/// The `APBDEV_RTC_MILLI_SECONDS_0` register.
+const RTC_MILLI_SECONDS: &'static ReadWrite<u32> =
+    unsafe { &(*((RTC_BASE + 0x10) as *const ReadWrite<u32>)) };
 
 /// Returns the current time in seconds.
+#[inline]
 pub fn get_seconds() -> u32 {
-    let rtc_seconds = unsafe { &(*(0x7000_E008 as *const ReadWrite<u32>)) };
-
-    rtc_seconds.get()
+    RTC_SECONDS.get()
 }
 
 /// Returns the current time in milliseconds.
+#[inline]
 pub fn get_milliseconds() -> u32 {
-    let rtc_milli_seconds = unsafe { &(*(0x7000_E010 as *const ReadWrite<u32>)) };
-    let rtc_shadow_seconds = unsafe { &(*(0x7000_E00C as *const ReadWrite<u32>)) };
-
-    rtc_milli_seconds.get() | (rtc_shadow_seconds.get() << 10)
+    RTC_MILLI_SECONDS.get() | (RTC_SHADOW_SECONDS.get() << 10)
 }
 
 /// Returns the current time in microseconds.
+#[inline]
 pub fn get_microseconds() -> u32 {
-    let timerus_cntr_1us_0 = unsafe { &(*(0x6000_5010 as *const ReadWrite<u32>)) };
-
-    timerus_cntr_1us_0.get()
+    TIMERUS_CNTR_1US.get()
 }
 
 /// Sleeps for a given duration in seconds.
+#[inline]
 pub fn sleep(duration: u32) {
-    let rtc_seconds = unsafe { &(*(0x7000_E008 as *const ReadWrite<u32>)) };
+    let start = get_seconds();
 
-    let start = rtc_seconds.get();
-
-    while (rtc_seconds.get() - start) < duration {}
+    while (get_seconds() - start) < duration {}
 }
 
 /// Sleeps for a given duration in milliseconds.
+#[inline]
 pub fn msleep(duration: u32) {
-    let rtc_milli_seconds = unsafe { &(*(0x7000_E010 as *const ReadWrite<u32>)) };
-    let rtc_shadow_seconds = unsafe { &(*(0x7000_E00C as *const ReadWrite<u32>)) };
+    let start = get_milliseconds();
 
-    let start = rtc_milli_seconds.get() | (rtc_shadow_seconds.get() << 10);
-
-    while ((rtc_milli_seconds.get() | (rtc_shadow_seconds.get() << 10)) - start) < duration {}
+    while (get_milliseconds() - start) < duration {}
 }
 
 /// Sleeps for a given duration in microseconds.
+#[inline]
 pub fn usleep(duration: u32) {
-    let timerus_cntr_1us_0 = unsafe { &(*(0x6000_5010 as *const ReadWrite<u32>)) };
+    let start = get_microseconds();
 
-    let start = timerus_cntr_1us_0.get();
-
-    while (timerus_cntr_1us_0.get() - start) < duration {}
+    while (get_microseconds() - start) < duration {}
 }
